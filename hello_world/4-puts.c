0ce73e5847a5d3b75a032b71bc04ed505c205d4c
damaske <damaske.vzk@gmail.com>
2024-10-24 10:55:43 +0000

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - enter point
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
