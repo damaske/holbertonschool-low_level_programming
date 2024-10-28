6ffbef257f9ebef81eeb2ee6d8c0bae43ae57048
damaske <damaske.vzk@gmail.com>
2024-10-28 17:27:07 +0000

    11. 98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - Prints all natural numbers from n to 98
@@ -6,6 +7,7 @@
  */
 void print_to_98(int n)
 {
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
@@ -25,4 +27,4 @@ void print_to_98(int n)
 				printf("%d\n", n);
 		}
 	}
-
+}
