770da11dd1df1c8849d368a2a9a6310e49188165
damaske <damaske.vzk@gmail.com>
2024-11-01 20:00:02 +0000

    push

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,5 +2,18 @@
 #define MAIN_H
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+
+/** 8 */
+void print_square(int size);
+/** 10*/
+
+void print_triangle(int size);
 
 #endif /* MAIN_H */
