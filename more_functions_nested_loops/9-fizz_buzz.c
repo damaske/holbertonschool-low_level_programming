1c6c87c45b178c4014931b33ce9b4fe1c0b4cc47
damaske <damaske.vzk@gmail.com>
2024-11-03 15:21:06 +0000

    9. Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description:Prints numbers from 1to100, replacing multiples of 3 with "Fizz"
+ * multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
+ * Return: 0 on success
+ */
+
+void main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else if  (n % 15 == 0)
+		{
+			printf("FizzBuzz\n");
+		else
+		{
+			printf("%d\n", n);
+
+	}
+}
