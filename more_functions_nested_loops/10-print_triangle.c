dae034d2220d5788f599ee65a86a16f13c999343
damaske <damaske.vzk@gmail.com>
2024-11-03 16:12:34 +0000

    10. Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle
+ * @size: The size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, k;
+
+		for (k = 0; k < size; k++)
+		{
+			for (i = 0; i < (size - k - 1); i++)
+			{
+				_putchar(' ');
+			}
+				_putchar('#');
+				_putchar('\n');
+		}
+	}
+}
