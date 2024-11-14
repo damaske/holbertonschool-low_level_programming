d463e55402ff8778c00ca7618e010a100151b20b
damaske <damaske.vzk@gmail.com>
2024-11-14 13:27:31 +0000

    1. strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: The destination string,must have enough space to holdtheresult.
+ * @src: The source string to be appended to dest.
+ * @n - bytes from src.
+ * This function appends the src string to the dest string, overwriting
+ * the terminating null byte (\0) at the end of dest, and then adds
+ * a new terminating null byte.
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int src_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (src[src_len] <= n)
+	{
+		dest[dest_len + src_len] = src[src_len];
+		src_len++;
+	}
+	return (dest);
+}
