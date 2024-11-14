c26d93f6e13eb9fa2dfc9bcdffde075161d51aef
damaske <damaske.vzk@gmail.com>
2024-11-14 15:09:53 +0000

    2. strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	while (src[i] != '\0' && n > i)
 	{
 		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
