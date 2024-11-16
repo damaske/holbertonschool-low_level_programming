33f2f0e1e27cfbb915935df7bf5ea35bb0c58bae
damaske <damaske.vzk@gmail.com>
2024-11-16 18:41:14 +0000

    2. strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,16 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 			s++;
 	}
 
-		if (c == '\0')
-			return (s);
-		return (NULL);
-}
+	if (c == '\0')
+	{
+		return (s);
+	}
 
+	return (NULL);
+}
