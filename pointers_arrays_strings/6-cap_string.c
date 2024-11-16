f1a877fd322f239ba6a0a96c50a97d48ac777ef8
damaske <damaske.vzk@gmail.com>
2024-11-16 11:02:48 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,26 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int is_separator = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' ||
-		str[i - 1] == '\t' ||
-		str[i - 1] == '\n' ||
-		str[i - 1] == ',' ||
-		str[i - 1] == ';' ||
-		str[i - 1] == '.' ||
-		str[i - 1] == '!' ||
-		str[i - 1] == '?' ||
-		str[i - 1] == '"' ||
-		str[i - 1] == '(' ||
-		str[i - 1] == ')' ||
-		str[i - 1] == '{' ||
-		str[i - 1] == '}' ||
-		i == 0)
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (i == 0 || str[i - 1] == separators[j])
+			{
+				is_separator = 1;
+				break;
+			}
+		}
+
+		if (is_separator && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
-
-			i++;
 		}
+		i++;
 	}
 	return (str);
 }
