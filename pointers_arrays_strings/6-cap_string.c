ae3e488512f677b45c78df24eb15f74098cdab51
damaske <damaske.vzk@gmail.com>
2024-11-16 11:23:36 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be modified.
@@ -9,24 +8,28 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int j;
 	char separators[] = " \t\n,;.!?\"(){}";
-	int is_separator = 0;
 
 	while (str[i] != '\0')
 	{
-		for (j = 0; separators[j] != '\0'; j++)
+		if (i == 0 || str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
 		{
-			if (i == 0 || str[i - 1] == separators[j])
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				is_separator = 1;
-				break;
+				str[i] -= 32;
 			}
-		}
-
-		if (is_separator && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] -= 32;
 		}
 		i++;
 	}
