fcdb317cc7ec4980cceabf2b5bf890b452a2bd32
damaske <damaske.vzk@gmail.com>
2024-11-16 11:49:36 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,26 +9,29 @@ char *cap_string(char *str)
 {
 	int i = 0;
 	char separators[] = " \t\n,;.!?\"(){}";
+	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		if (i == 0 || str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
+		if (i == 0)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] -= 32;
+			}
+		}
+		else
+		{
+			for (j = 0; separators[j] != '\0'; j++)
+			{
+				if (str[i - 1] == separators[j])
+				{
+					if (str[i] >= 'a' && str[i] <= 'z')
+					{
+						str[i] -= 32;
+					}
+					break;
+				}
 			}
 		}
 		i++;
