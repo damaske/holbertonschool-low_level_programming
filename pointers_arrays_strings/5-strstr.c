b70cf964751aeb7b3975216d5c6e9d28c6795e28
damaske <damaske.vzk@gmail.com>
2024-11-17 19:55:09 +0000

    5. strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - Locates the first occurrence of a substring in a string.
+ * @haystack: The string to be searched.
+ * @needle: The substring to locate in haystack.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		char *start = haystack;
+		char *sub = needle;
+		
+		while (*start == *sub && *sub != '\0')
+		{
+			start++;
+			sub++;
+		}
+		if (*sub == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
