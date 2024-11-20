83b6db6ba35513cd39cf156f0c13ed8890111b46
damaske <damaske.vzk@gmail.com>
2024-11-20 20:16:40 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.>
+#include <stddef.h>
 /**
  * _puts_recursion - Prints a string followed by a new line.
  * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-	if (s == NULL) {
+	if (s == NULL) 
+	{
 		return;
 	}
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
