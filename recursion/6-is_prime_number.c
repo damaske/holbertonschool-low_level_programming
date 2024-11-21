df2b8e684808d5fcb5ed81297f4057b15293645a
damaske <damaske.vzk@gmail.com>
2024-11-21 20:05:21 +0000

    6. Inception. Is it possible?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,30 @@
 #include "main.h"
 /**
- * is_prime_number - Determines if a number is a prime number.
- * @n: The number to check.
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * Description: This function checks if the input integer is a prime number.
- * It returns 1 if the number is prime, otherwise it returns 0.
- *
- * Return: 1 if `n` is a prime number, 0 otherwise.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int is_prime_helper(int n, int i)
+int prime2(int a, int b)
 {
-    if (i * i > n)
-        return (1);
-    if (n % i == 0)
-        return (0);
-    return (is_prime_helper(n, i + 1));
+if (a == b)
+return (1);
+else if (a % b == 0)
+return (0);
+return (prime2(a, b + 1));
 }
-
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    return (is_prime_helper(n, 2));
+if (n <= 1)
+return (0);
+return (prime2(n, 2));
 }
-
