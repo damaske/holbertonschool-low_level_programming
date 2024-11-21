3a55f6b3cc36f333b1ca18bbbc9de3afe5da1107
damaske <damaske.vzk@gmail.com>
2024-11-21 20:03:57 +0000

    6. Inception. Is it possible?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,20 +8,19 @@
  *
  * Return: 1 if `n` is a prime number, 0 otherwise.
  */
+int is_prime_helper(int n, int i)
+{
+    if (i * i > n)
+        return (1);
+    if (n % i == 0)
+        return (0);
+    return (is_prime_helper(n, i + 1));
+}
+
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+    if (n <= 1)
+        return (0);
+    return (is_prime_helper(n, 2));
 }
+
