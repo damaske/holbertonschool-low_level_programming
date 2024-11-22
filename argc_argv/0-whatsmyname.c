79eff2b830556027b3186182fec00ebb72b1ce96
damaske <damaske.vzk@gmail.com>
2024-11-22 12:59:37 +0000

    0. It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: Argument vector (array of strings containing the arguments).
  * Return: Always 0 (Success).
  */
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
