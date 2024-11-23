628fd4fe082a007dd69430373fe24f2078231f24
damaske <damaske.vzk@gmail..com>
2024-11-23 13:06:34 +0000

    3. If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,28 +10,22 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, **grid;
-	
-	if (width <= 0 || height <= 0)
-		return (NULL);
-	
-	grid = malloc(height * sizeof(int *));
-	
-	if (grid == NULL)
-		return (NULL);
-	
-	for (i = 0; i < height; i++)
-	
-	{
-		grid[i] = malloc(width * sizeof(int));
-		
-		if (grid[i] == NULL);
-		{
-				for (j = 0; j < i; j++)
-				free(grid[j]);
-				free(grid);
-				return (NULL);
-				}
-				}
-				return (grid);
-			}
+int i, j, **grid;
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+}
+return (grid);
+}			}
