052f1780d64e88168b29cd5abb8000a3e1f5f1a0
damaske <damaske.vzk@gmail..com>
2024-11-23 14:50:55 +0000

    0. Trust no one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ * Return: A pointer to the allocated memory.
+ * If malloc fails, the process terminates with status 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *som;
+
+	sam = malloc(b);
+
+	if (sam == NULL)
+	{
+		exit(98);
+	}
+	return (sam);
+}
