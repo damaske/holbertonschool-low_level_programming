ff6652414a34a2f6402bba70f7d6f018ea89a334
damaske <damaske.vzk@gmail..com>
2024-11-23 12:34:34 +0000

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,19 +16,17 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = "";
-		len1 = 0;
 	}
 	if (s2 == NULL)
 	{
 		s2 = "";
-		len2 = 0;
 	}
 	len1 = 0;
-	while  (*s1 != '\0')
+	while  (s1[len1] != '\0')
 	{
 		len1++;
 	}
-	while (*s2 != '\0')
+	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
