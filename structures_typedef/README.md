22d8b7411c48fd13bb50046ec5735f27584c1b9f
damaske <damaske.vzk@gmail.com>
2024-11-26 20:11:28 +0000

    0. Poppy

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+ufhntfhtf:x
+
---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
