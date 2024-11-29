9573614cc1a8c55a34c1c43fd8f277caa87ab57e
damaske <damaske.vzk@gmail.com>
2024-11-29 12:29:14 +0000

    2. To hell with circumstances; I create opportunities

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FPOINTERS_H
 #define FPOINTERS_H
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
