4e426f9887a2819c6193572b58b90bbd24b05339
damaske <damaske.vzk@gmail.com>
2024-11-30 15:27:26 +0000

    2. To hell with circumstances; I create opportunities

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,10 +6,9 @@
  * @array: The array of integers.
  * @size: The size of the array.
  * @cmp: A pointer to the function to be used to compare values.
- *
  * Return: If no element matches or size <= 0 - -1.
- *         Otherwise - The index of the first element for which
- *                     the cmp function does not return 0.
+ * Otherwise - The index of the first element for which
+ * the cmp function does not return 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
