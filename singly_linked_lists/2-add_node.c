cced4149c113fe4af9c59b49563fbdb319ec4ec6
damaske <damaske.vzk@gmail.com>
2024-12-07 21:47:04 +0000

    2. Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -39,7 +39,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = copy_str;
-	new_node->len = strlen(copy_str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
