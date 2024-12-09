125c0d75b388f7a84ef22063c08efc4dbf992b74
damaskee <damaske.vzk@gmail.com>
2024-12-09 13:47:41 +0000

    0. 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,8 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 
 	if (b == NULL)
-		return (NULL);
-	for (; b != "\0"; b++)
+		return (0);
+	for (; b != '\0'; b++)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
